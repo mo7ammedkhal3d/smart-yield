a0b1412b2aec23a476bc4fb38755a8b5e5ecbc6c
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-10-31 23:30:43 +0300

    Comp Teask 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './assets/investment-calculator-logo.png';
 import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
-
+    
     const yearlyData = []; // per-year results
+    
 
     let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
@@ -26,46 +28,20 @@ function App() {
         yearlyContribution: yearlyContribution,
       });
     }
+    
+
 
     // do something with yearlyData ...
+
+    console.log(yearlyData);
+    
+
   };
 
   return (
     <div>
       <Header logo={logo}/>
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
-
+      <Form onSaveChanges={calculateHandler}/>
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
